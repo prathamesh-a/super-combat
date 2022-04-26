54e8f404f2647dc65ad6a7b45f916b6a4173517b
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-04-26 17:09:53 +0530

    create entity

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext("2d")
 
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.valocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.valocity.y
+
+        if (this.position.y + this.height + this.valocity.y >= canvas.height){
+            this.valocity.y = 0
+        }
+        else this.valocity.y += gravity
+    }
+}
+
+const player = new Sprite({
+    position:{
+        x: 0,
+        y: 0
+    },
+    velocity:{
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position:{
+        x: 400,
+        y: 100
+    },
+    velocity:{
+        x: 0,
+        y: 0
+    }
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
