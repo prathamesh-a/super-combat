c688f9e6a4ded5fcd50fdacf4e255c76c02709ae
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-04-26 18:19:42 +0530

    entity movement

--- index.js
+++ index.js
@@ -3,28 +3,48 @@ const ctx = canvas.getContext("2d")
 
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.5
+const playerSpeed = 4;
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color}) {
         this.position = position
-        this.valocity = velocity
+        this.velocity = velocity
+        this.width = 50
         this.height = 150
+        this.lastKey
+        this.isOnGround
+        this.color = color
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
     }
 
     draw() {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+        // Character Box
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // Attack Box
+        ctx.fillStyle = 'yellow'
+        ctx.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
         this.draw()
-        this.position.y += this.valocity.y
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.valocity.y >= canvas.height){
-            this.valocity.y = 0
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+            this.isOnGround = true
         }
-        else this.valocity.y += gravity
+        else {
+            this.velocity.y += gravity
+            this.isOnGround = false;
+        }
     }
 }
 
@@ -36,7 +56,8 @@ const player = new Sprite({
     velocity:{
         x: 0,
         y: 0
-    }
+    },
+    color: 'red'
 })
 
 const enemy = new Sprite({
@@ -47,8 +68,24 @@ const enemy = new Sprite({
     velocity:{
         x: 0,
         y: 0
-    }
+    },
+    color: 'blue'
 })
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -56,6 +93,78 @@ function animate() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    // Player Movement
+    player.velocity.x = 0
+    if (keys.a.pressed && player.lastKey === 'a' && player.position.x > 0) {
+        player.velocity.x = -playerSpeed
+    } else if (keys.d.pressed && player.lastKey === 'd' && player.position.x < canvas.width-50){
+        player.velocity.x = playerSpeed
+    }
+
+    // Enemy Movement
+    enemy.velocity.x = 0
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft' && enemy.position.x > 0) {
+        enemy.velocity.x = -playerSpeed
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight' && enemy.position.x < canvas.width-50){
+        enemy.velocity.x = playerSpeed
+    }
+
+    // Collision Detection
+    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x
+        && player.attackBox.position.x <= enemy.position.x + enemy.width
+        && player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+        && player.attackBox.position.y <= enemy.position.y + enemy.height) {
+        console.log("Red attacked Blue!")
+    }
 }
 
 animate()
+
+window.addEventListener('keydown', (e)=>{
+    switch (e.key){
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+        case 'w':
+            if (player.isOnGround) player.velocity.y = -17
+            break
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            if (enemy.isOnGround) enemy.velocity.y = -17
+            break
+    }
+})
+
+window.addEventListener('keyup', (e)=>{
+    switch (e.key){
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+
+    switch (e.key){
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+    }
+})
